c3bc8c30450ed99d93bf96df2c5c86072ad19855
Gustaf Holm <gustafholm1@gmail.com>
2017-10-03 13:24:39 +0200

    Fix lint errors on type  either

--- types/either.js
+++ types/either.js
@@ -4,18 +4,18 @@ const Left = x => ({
   chain: f => f(x),
   fold: (f, g) => f(x),
   map: f => Left(x)
-});
+})
 
 const Right = x => ({
   chain: f => f(x),
   fold: (f, g) => g(x),
   map: f => Right(f(x))
-});
+})
 
 Either.Left = Left
 Either.Right = Right
 
-Eiter.fromNullable = x => (x == null || x == undefined ? Left(x) : Right(x))
+Either.fromNullable = x => (x === null || x === undefined ? Left(x) : Right(x))
 Either.of = x => Right(x)
 
 module.exports = Either
