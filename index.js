d1c5509bbfe03e86f6729eee38b398d486644bd3
Gustaf Holm <gustafholm1@gmail.com>
2017-10-03 13:01:25 +0200

    Update and export from index.js

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const curry = require('./curry')
 const compose = require('./compose')
+const curry = require('./curry')
 const pipe = require('./pipe')
 
-const Sum = require('./types/sum')
+const All = require('./types/all')
+const Any = require('./types/any')
+const Either = require('./types/either')
 const Product = require('./types/product')
+const Sum = require('./types/sum')
+
+module.exports = {
+  compose,
+  curry,
+  pipe,
+  All,
+  Any,
+  Either,
+  Product,
+  Sum
+}
