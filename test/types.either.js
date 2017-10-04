5bede382a0a4923e4b010db3b17bb94dc407329e
Gustaf Holm <gustafholm1@gmail.com>
2017-10-04 08:28:28 +0200

    updated test on type either

--- test/types.either.js
+++ test/types.either.js
@@ -1,44 +1,40 @@
 const tap = require('tap')
 const { Either } = require('../')
 
-tap.test('either fold left on null', (t) => {
-  const found = Either.fromNullable(null).fold(l => l, r => r)
-  const wanted = null
+tap.test('either fromNullable  fold left on null', (t) => {
+  const found = Either.fromNullable(null).fold(l => 'left', r => 'right')
+  const wanted = 'left'
 
   t.equal(found, wanted)
   t.end()
 })
 
-tap.test('either fold left on undefined', (t) => {
-  const found = Either.fromNullable(undefined).fold(l => l, r => r)
-  const wanted = undefined
+tap.test('either fromNullable fold left on undefined', (t) => {
+  const found = Either.fromNullable(undefined).fold(l => 'left', r => 'right')
+  const wanted = 'left'
 
   t.equal(found, wanted)
   t.end()
 })
 
-tap.test('either fold right on not null', (t) => {
-  const found = Either.fromNullable('success').fold(l => l, r => r)
-  const wanted = 'success'
+tap.test('either fromNullable  fold right on not null', (t) => {
+  const found = Either.fromNullable('success').fold(l => 'left', r => 'right')
+  const wanted = 'right'
 
   t.equal(found, wanted)
   t.end()
 })
 
 tap.test('either of fold right', (t) => {
-  const found = Either.of({ hello: 'world' })
-    .fold(l => l, r => r)
-
-  const wanted = { hello: 'world' }
+  const found = Either.of({ hello: 'world' }).fold(l => 'left', r => 'right')
+  const wanted = 'right'
 
   t.deepEqual(found, wanted)
   t.end()
 })
 
 tap.test('either left fold left', (t) => {
-  const found = Either.Left(null)
-    .fold(l => 'left', r => 'right')
-
+  const found = Either.Left(null).fold(l => 'left', r => 'right')
   const wanted = 'left'
 
   t.deepEqual(found, wanted)
@@ -46,9 +42,7 @@ tap.test('either left fold left', (t) => {
 })
 
 tap.test('either right fold right', (t) => {
-  const found = Either.Right(null)
-    .fold(l => 'left', r => 'right')
-
+  const found = Either.Right(null).fold(l => 'left', r => 'right')
   const wanted = 'right'
 
   t.deepEqual(found, wanted)
