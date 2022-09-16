e595407fea9e1ac937a3d713db6f44d38da19e2b
Primalivet <gustaf.holn@icloud.com>
2022-09-16 15:34:26 +0200

    New implementation of Either

--- src/either.test.ts
+++ src/either.test.ts
@@ -1,158 +1,206 @@
-import test from 'ava'
+// import test from 'jest'
+import { pipe } from './function'
 import * as E from './either'
 
-test('left identity', (t) => {
-  const f = (x: number) => E.of(x * 2)
-  const leftside = E.chain(f)(E.of(10))
-  const rightside = f(10)
-  const found = E.show(leftside) === E.show(rightside)
-  const wanted = true
+describe('Laws', () => {
+  test('left identity', () => {
+    const f = (x: number) => E.pure(x * 2)
+    const leftside = E.bind(f)(E.pure(10))
+    const rightside = f(10)
+    const found = E.show(leftside) === E.show(rightside)
+    const wanted = true
 
-  t.is(found, wanted)
+    expect(found).toBe(wanted)
+  })
+
+  test('right identity', () => {
+    const leftside = E.bind(E.pure)(E.pure(10))
+    const rightside = E.pure(10)
+    const found = E.show(leftside) === E.show(rightside)
+    const wanted = true
+
+    expect(found).toBe(wanted)
+  })
+
+  test('associativity', () => {
+    const f = (x: number) => E.pure(x * 2)
+    const g = (x: number) => E.pure(x + 1)
+    const leftside = E.bind(g)(E.bind(f)(E.pure(10)))
+    const rightside = E.bind((x: number) => E.bind(g)(f(x)))(E.pure(10))
+    const found = E.show(leftside) === E.show(rightside)
+    const wanted = true
+
+    expect(found).toBe(wanted)
+  })
 })
 
-test('right identity', (t) => {
-  const leftside = E.chain(E.of)(E.of(10))
-  const rightside = E.of(10)
-  const found = E.show(leftside) === E.show(rightside)
-  const wanted = true
+describe('Constructors', () => {
+  test('left', () => {
+    const found = pipe(E.left('reason'), E.show)
+    const wanted = 'Left("reason")'
 
-  t.is(found, wanted)
+    expect(found).toBe(wanted)
+  })
+
+  test('right', () => {
+    const found = pipe(E.right('success'), E.show)
+    const wanted = 'Right("success")'
+
+    expect(found).toBe(wanted)
+  })
+
+  test('fromNullable: Left when value is null', () => {
+    const found = pipe(
+      null,
+      E.fromNullable((x) => x),
+    )
+    const wanted = E.left(null)
+
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
+
+  test('fromNullable: Left when value is undefined', () => {
+    const found = pipe(
+      undefined,
+      E.fromNullable((x) => x),
+    )
+    const wanted = E.left(undefined)
+
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
+
+  test('fromNullable: Right when value is not null or undefined', () => {
+    const found = pipe(
+      'hello world',
+      E.fromNullable((x) => x),
+    )
+    const wanted = E.right('hello world')
+
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
+
+  test('fromPredicate: Right when predicate is true', () => {
+    const found = pipe(
+      ['hello', 'world'],
+      E.fromPredicate(Array.isArray)((x) => x),
+    )
+    const wanted = E.right(['hello', 'world'])
+
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
+
+  test('fromPredicate: Left when predicate is false', () => {
+    const found = pipe(
+      'hello world',
+      E.fromPredicate(Array.isArray)((x) => x),
+    )
+    const wanted = E.left('hello world')
+
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
+
+  test('tryCatch: Right when function does not throw', () => {
+    const found = E.tryCatch(
+      () => {
+        throw new Error('failure')
+      },
+      (x) => x,
+    )
+    const wanted = E.left(new Error('failure'))
+
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
+
+  test('tryCatch: Left when function does throw', () => {
+    const found = E.tryCatch(
+      () => {
+        return 'success'
+      },
+      (x) => x,
+    )
+    const wanted = E.right('success')
+
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
 })
 
-test('associativity', (t) => {
-  const f = (x: number) => E.of(x * 2)
-  const g = (x: number) => E.of(x + 1)
-  const leftside = E.chain(g)(E.chain(f)(E.of(10)))
-  const rightside = E.chain((x:number) => E.chain(g)(f(x)))(E.of(10))
-  const found = E.show(leftside) === E.show(rightside)
-  const wanted = true
+describe('Destructors', () => {
+  test('fold: calls onLeft on left value', () => {
+    const found = E.fold(
+      () => 'failure',
+      () => 'success',
+    )(E.left(''))
+    const wanted = 'failure'
+    expect(found).toBe(wanted)
+  })
 
-  t.is(found, wanted)
+  test('fold: calls onRight on right value', () => {
+    const found = E.fold(
+      () => 'failure',
+      () => 'success',
+    )(E.right(''))
+    const wanted = 'success'
+    expect(found).toBe(wanted)
+  })
 })
 
-// tap.test('Either fromNullable fold Left on null', t => {
-//   const found = Either.fromNullable(null).inspect()
-//   const wanted = 'Left(null)'
+describe('Functor', () => {
+  test('fmap: maps the function over a right value', () => {
+    const double = (x: number) => x * 2
+    const found = E.fmap(double)(E.right(2))
+    const wanted = E.right(4)
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
 
-//   t.equal(found, wanted)
-//   t.end()
-// })
+  test('fmap: dont do anything on a left value', () => {
+    const double = (x: number) => x * 2
+    const found = E.fmap(double)(E.left(2))
+    const wanted = E.left(2)
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
+})
 
-// tap.test('Either fromNullable fold Left on undefined', t => {
-//   const found = Either.fromNullable(undefined).inspect()
-//   const wanted = 'Left(undefined)'
+describe('Applicative', () => {
+  test('pure: wrapps a value in the context', () => {
+    const found = E.pure(2)
+    const wanted = E.right(2)
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
 
-//   t.equal(found, wanted)
-//   t.end()
-// })
+  test('apply: apply the right value to the function', () => {
+    const double = (x: number) => x * 2
+    const found = E.apply(E.right(2))(E.right(double))
+    const wanted = E.right(4)
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
 
-// tap.test('Either fromNullable fold Right on not null', t => {
-//   const found = Either.fromNullable('success').inspect()
-//   const wanted = 'Right(success)'
+  test('apply: dont do anything on a left value', () => {
+    const double = (x: number) => x * 2
+    const found = E.apply(E.right(2))(E.left(double))
+    const wanted = E.left(double)
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
 
-//   t.equal(found, wanted)
-//   t.end()
-// })
+  test('apply: dont do anything on a left function', () => {
+    const double = (x: number) => x * 2
+    const found = E.apply(E.left(2))(E.right(double))
+    const wanted = E.left(2)
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
+})
 
-// tap.test('Either tryCatch fold Right on success', t => {
-//   const f = () => 'success'
-//   const found = Either.tryCatch(f).inspect()
-//   const wanted = 'Right(success)'
+describe('Monad', () => {
+  test('bind: apply the right value to a function returning an either, then flatten the result', () => {
+    const mDouble = (x: number) => E.pure(x * 2)
+    const found = E.bind(mDouble)(E.pure(2))
+    const wanted = E.right(4)
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
 
-//   t.equal(found, wanted)
-//   t.end()
-// })
-
-// tap.test('Either tryCatch fold Left on error', t => {
-//   const f = () => { throw new Error('failure') }
-//   const found = Either.tryCatch(f).inspect()
-//   const wanted = 'Left(Error: failure)'
-
-//   t.equal(found, wanted)
-//   t.end()
-// })
-
-// tap.test('Either of fold Right', t => {
-//   const found = Either.of('success').inspect()
-//   const wanted = 'Right(success)'
-
-//   t.equal(found, wanted)
-//   t.end()
-// })
-
-// tap.test('Either Left fold Left', t => {
-//   const found = Either.Left(null).inspect()
-//   const wanted = 'Left(null)'
-
-//   t.equal(found, wanted)
-//   t.end()
-// })
-
-// tap.test('Either Right fold Right', t => {
-//   const found = Either.Right(null).inspect()
-//   const wanted = 'Right(null)'
-
-//   t.equal(found, wanted)
-//   t.end()
-// })
-
-// tap.test('Either Right map method does run', t => {
-//   const found = Either.Right(10)
-//     .map(x => x * 10)
-//     .inspect()
-//   const wanted = 'Right(100)'
-
-//   t.equal(found, wanted)
-//   t.end()
-// })
-
-// tap.test('Either Left map method is does NOT run', t => {
-//   const found = Either.Left(10)
-//     .map(x => x * 10)
-//     .inspect()
-//   const wanted = 'Left(10)'
-
-//   t.equal(found, wanted)
-//   t.end()
-// })
-
-// tap.test('Either Right chain method does run and unfolds nested type', t => {
-//   const found = Either.Right(10)
-//     .chain(x => Either.fromNullable(x * 10))
-//     .inspect()
-//   const wanted = 'Right(100)'
-
-//   t.equal(found, wanted)
-//   t.end()
-// })
-
-// tap.test('Either Left chain method does NOT run', t => {
-//   const found = Either.Left(10)
-//     .chain(x => Either.fromNullable(x * 10))
-//     .inspect()
-//   const wanted = 'Left(10)'
-
-//   t.equal(found, wanted)
-//   t.end()
-// })
-
-// tap.test('Either Left ap method does NOT run', t => {
-//   const found = Either.Left(x => x * 2)
-//     .ap(Either.of(10))
-//     .inspect()
-//   const wanted = 'Left(x => x * 2)'
-
-//   t.equal(found, wanted)
-//   t.end()
-// })
-
-// tap.test('Either Right ap method does run', t => {
-//   const found = Either.Right(x => x * 2)
-//     .ap(Either.of(10))
-//     .inspect()
-//   const wanted = 'Right(20)'
-
-//   t.equal(found, wanted)
-//   t.end()
-// })
+  test('bind: dont do anything to a left value', () => {
+    const mDouble = (x: number) => E.pure(x * 2)
+    const found = E.bind(mDouble)(E.left(2))
+    const wanted = E.left(2)
+    expect(E.show(found)).toBe(E.show(wanted))
+  })
+})
--- src/either.ts
+++ src/either.ts
@@ -1,53 +1,117 @@
+/*
+ * Either is a union type of Left or Right. This means that an either can hold
+ * two different semantic values while still beeing one type. By convention,
+ * Left is generally used for a failure or error. It's main strenght is to
+ * handle errors.
+ */
 export type Either<A, B> = Left<A> | Right<B>
 
-type Left<A> = { _tag: 'Left'; value: A }
+/*
+ * A Left type is one of two parts in the Either union type. It generally holds
+ * a reason for failure, error or alike.
+ */
+export type Left<A> = { _tag: 'Left'; value: A }
 
-type Right<A> = { _tag: 'Right'; value: A }
+/*
+ * A Right type is one of two parts in the Either union type. It generally holds
+ * a value from a successfull computation.
+ */
+export type Right<A> = { _tag: 'Right'; value: A }
 
+/*
+ * Constructor for a Left value.
+ */
 export const left = <A>(a: A): Left<A> => ({ _tag: 'Left', value: a })
 
+/*
+ * Constructor for a Right value.
+ */
 export const right = <A>(a: A): Right<A> => ({ _tag: 'Right', value: a })
-export const of = right
 
+/*
+ * Constructor of the Either identity value, which is a Right. An identity type
+ * can be described as a value that wont change the result while being combined
+ * with another value of the same type.
+ *
+ * For example
+ * 2 + 0 = 0 (in addition, 0 is the identity value)
+ * 2 * 1 = 2 (in multiplication, 1 is the identity value)
+ *
+ * For an either, right is the identity value as a Left combined with an Right
+ * will result in a Left, no mather how it's combined. While a Right combined
+ * width a Left will change the Right into a Left.
+ */
+export const pure = right
+
+/*
+ * Predicate for checking if the Either is a Left
+ */
 export const isLeft = <A, B>(ma: Either<A, B>): boolean => ma._tag === 'Left'
 
+/*
+ * Predicate for checking if the Either is a Right
+ */
 export const isRight = <A, B>(ma: Either<A, B>): boolean => ma._tag === 'Right'
 
-export const map =
+/*
+ * The fmap operation of an Either.
+ * On a Right value, map will apply it's value to the given function.
+ * On a Left value, map will ignore the function and keep it's Left value.
+ */
+export const fmap =
   <A, B, C>(f: (b: B) => C) =>
   (ma: Either<A, B>): Either<A, C> =>
     ma._tag === 'Right' ? right(f(ma.value)) : ma
 
-export const chain =
+/*
+ * The bind operation of an Either.
+ * On a Right value, bind will apply it's value to the given function, and
+ * flatten the result as the function has to return another Either.
+ * On a Left value, bind will ignore the function and keep it's Left value.
+ */
+export const bind =
   <A, B, C>(f: (b: B) => Either<A, C>) =>
   (ma: Either<A, B>): Either<A, C> =>
     ma._tag === 'Right' ? f(ma.value) : ma
 
-export const ap =
+/*
+ * The apply operation is mush the reverse fmap operation.
+ * The Right value holds a argument if you will that will be applied to the
+ * given context holding a function
+ * Short circuted on any Left value
+ */
+export const apply =
   <E, A>(fa: Either<E, A>) =>
-  <B>(fab: Either<E, (a: A) => B>) =>
+  <F, B>(fab: Either<F, (a: A) => B>): Either<E | F, A | B> =>
     fab._tag === 'Left'
       ? fab
       : fa._tag === 'Left'
       ? fa
       : right(fab.value(fa.value))
 
-export const match =
-  <A, B, C>(onLeft: (a: A) => C, onRight: (b: B) => C) =>
-  (ma: Either<A, B>): C =>
-    ma._tag === 'Left' ? onLeft(ma.value) : onRight(ma.value)
-
+/*
+ * Constuct a Either from a possible null value
+ * Left on null or undefined and otherwise Right
+ */
 export const fromNullable =
   <A, B>(onNullable: (b: B) => A) =>
   (b: B) =>
     b === null || b === undefined ? left(onNullable(b)) : right(b)
 
+/*
+ * Constuct a Either from a Predicate
+ * When the predicate is true, wrap the value in a Right otherwise Left
+ */
 export const fromPredicate =
   <A, B>(predicate: (b: B) => boolean) =>
   (onUnsatisfied: (b: B) => A) =>
   (b: B): Either<A, B> =>
     predicate(b) ? right(b) : left(onUnsatisfied(b))
 
+/*
+ * Construct a Either from a thunk that might throw If it throws we get a Left
+ * with the reason, otherwise a Right with the value
+ */
 export const tryCatch = <A, B>(
   f: () => B,
   onError: (e: unknown) => A,
@@ -59,6 +123,18 @@ export const tryCatch = <A, B>(
   }
 }
 
+/*
+ * Extract the value from an Either by giving it two functions, one if it's a
+ * Left value and one if it's a Right value
+ */
+export const fold =
+  <A, B, C>(onLeft: (a: A) => C, onRight: (b: B) => C) =>
+  (ma: Either<A, B>): C =>
+    ma._tag === 'Left' ? onLeft(ma.value) : onRight(ma.value)
+
+/*
+ * Turn your Either into a string to print or inspect the value
+ */
 export const show = <A, B>(e: Either<A, B>): string =>
   e._tag === 'Left'
     ? `Left(${JSON.stringify(e.value, null, 2)})`
