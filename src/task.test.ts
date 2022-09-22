6c845484cb6a3afef3876cb28f08ce2426bb4b1d
Primalivet <gustaf.holn@icloud.com>
2022-09-22 19:08:12 +0200

    test(task): update test with composability and lower delay

--- src/task.test.ts
+++ src/task.test.ts
@@ -1,10 +1,11 @@
 import * as T from './task'
+import * as F from './function'
 
 const upper = (s: string) => s.toUpperCase()
 const asyncUpper =
   (s: string): T.Task<string> =>
   () =>
-    new Promise((resolve) => setTimeout(() => resolve(s.toUpperCase()), 1500))
+    new Promise((resolve) => setTimeout(() => resolve(s.toUpperCase()), 300))
 
 describe('Laws', () => {
   test('left identity', () => {
@@ -71,10 +72,29 @@ describe('Monad', () => {
 describe('Constructor', () => {
 
   test('delay', async () => {
-    const found = await T.delay(1500)('hello')()
+    const found = await T.delay(300)('hello')()
     const wanted = 'hello'
     expect(found).toBe(wanted)
   })
 })
 
 
+describe('Composable', () => {
+  test('compose', async () => {
+    const found = F.compose(T.fmap((x: number) => x * 2), T.pure<number>)(10)
+    const wanted = 20
+    expect(await found()).toBe(wanted)
+  })
+
+  test('pipe', async () => {
+    const found = F.pipe(10, T.pure, T.fmap((x: number) => x * 2))
+    const wanted = 20
+    expect(await found()).toBe(wanted)
+  })
+
+  test('flow', async () => {
+    const found = F.flow(T.pure<number>, T.fmap((x: number) => x * 2))(10 as const)
+    const wanted = 20
+    expect(await found()).toBe(wanted)
+  })
+})
