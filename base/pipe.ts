c0e4df0a130bf28b42a19c5651e6092cab03339b
Gustaf Holm <gustaf.holm@icloud.com>
2024-12-16 15:28:19 +0100

    feat(base): reimplement compose and add docs to base

--- base/pipe.ts
+++ base/pipe.ts
@@ -1,3 +1,42 @@
+/**
+ * Given some value and up to 11 function pipe creates a "pipeline", from left
+ * to right,  where the output of a function becomes the input to the next.
+ *
+ * @typeParam A The initial input value and input to the first function
+ * @typeParam B The output of the first function, input to the second
+ * @typeParam C The output of the second function, input to the third function if given, otherwise the return value
+ * @typeParam D The output of the third function, input to the fourth function if given, otherwise the return value
+ * @typeParam E The output of the fourth function, input to the fifth function if given, otherwise the return value
+ * @typeParam F The output of the fifth function, input to the sixth function if given, otherwise the return value
+ * @typeParam G The output of the sixth function, input to the seventh function if given, otherwise the return value
+ * @typeParam H The output of the seventh function, input to the eighth function if given, otherwise the return value
+ * @typeParam I The output of the eighth function, input to the ninth function if given, otherwise the return value
+ * @typeParam J The output of the ninth function, input to the tenth function if given, otherwise the return value
+ * @typeParam K The output of the tenth function, input to the eleventh function if given, otherwise the return value
+ * @typeParam L The output of the eleventh function, the return value
+ * @param a The initial input value
+ * @param ab function to transform input A to output B,
+ * @param bc function to transform input B to output C,
+ * @param cd function to transform input C to output D,
+ * @param de function to transform input D to output E,
+ * @param ef function to transform input E to output F,
+ * @param fg function to transform input F to output G,
+ * @param gh function to transform input G to output H,
+ * @param hi function to transform input H to output I,
+ * @param ij function to transform input I to output J,
+ * @param jk function to transform input J to output K,
+ * @param kl function to transform input K to output L,
+ * @returns A value transformed by the functions in the pipeline
+ *
+ * @example
+ * ```ts
+ * import { pipe } from '@gimme/base/pipe'
+ * const toLower = (s:string):string => s.toLowerCase()
+ * const split = (delimiter: string) => (s: string): string[] => s.split(delimiter)
+ * const join = (delimiter: string) => (s: string[]): string => s.join(delimiter)
+ * const snake_case = pipe("Hello World", toLower, split(" "), join("_"))
+ * ```
+ */
 export function pipe<A, B>(a: A, ab: (a: A) => B): B;
 export function pipe<A, B, C>(a: A, ab: (a: A) => B, bc: (b: B) => C): C;
 export function pipe<A, B, C, D>(
