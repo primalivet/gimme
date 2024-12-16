525279c541069516069513ac49126c23ecf7727a
Gustaf Holm <gustaf.holm@icloud.com>
2024-12-16 15:28:19 +0100

    feat(base): reimplement compose and add docs to base

--- base/compose.ts
+++ base/compose.ts
@@ -1,98 +1,122 @@
-export const compose =
-  <A, B, C>(bc: (b: B) => C, ab: (a: A) => B) => (a: A): C => bc(ab(a));
-
-export const compose3 =
-  <A, B, C, D>(cd: (c: C) => D, bc: (b: B) => C, ab: (a: A) => B) =>
-  (a: A): D => cd(bc(ab(a)));
-
-export const compose4 = <A, B, C, D, E>(
-  de: (d: D) => E,
-  cd: (c: C) => D,
-  bc: (b: B) => C,
-  ab: (a: A) => B,
-) =>
-(a: A): E => de(cd(bc(ab(a))));
-
-export const compose5 = <A, B, C, D, E, F>(
-  ef: (e: E) => F,
-  de: (d: D) => E,
-  cd: (c: C) => D,
-  bc: (b: B) => C,
-  ab: (a: A) => B,
-) =>
-(a: A): F => ef(de(cd(bc(ab(a)))));
-
-export const compose6 = <A, B, C, D, E, F, G>(
-  fg: (f: F) => G,
-  ef: (e: E) => F,
-  de: (d: D) => E,
-  cd: (c: C) => D,
-  bc: (b: B) => C,
-  ab: (a: A) => B,
-) =>
-(a: A): G => fg(ef(de(cd(bc(ab(a))))));
-
-export const compose7 = <A, B, C, D, E, F, G, H>(
-  gh: (g: G) => H,
-  fg: (f: F) => G,
-  ef: (e: E) => F,
-  de: (d: D) => E,
-  cd: (c: C) => D,
-  bc: (b: B) => C,
-  ab: (a: A) => B,
-) =>
-(a: A): H => gh(fg(ef(de(cd(bc(ab(a)))))));
-
-export const compose8 = <A, B, C, D, E, F, G, H, I>(
-  hi: (h: H) => I,
-  gh: (g: G) => H,
-  fg: (f: F) => G,
-  ef: (e: E) => F,
-  de: (d: D) => E,
-  cd: (c: C) => D,
-  bc: (b: B) => C,
-  ab: (a: A) => B,
-) =>
-(a: A): I => hi(gh(fg(ef(de(cd(bc(ab(a))))))));
-
-export const compose9 = <A, B, C, D, E, F, G, H, I, J>(
-  ij: (i: I) => J,
-  hi: (g: H) => I,
-  gh: (g: G) => H,
-  fg: (f: F) => G,
-  ef: (e: E) => F,
-  de: (d: D) => E,
-  cd: (c: C) => D,
-  bc: (b: B) => C,
-  ab: (a: A) => B,
-) =>
-(a: A): J => ij(hi(gh(fg(ef(de(cd(bc(ab(a)))))))));
-
-export const compose10 = <A, B, C, D, E, F, G, H, I, J, K>(
-  jk: (j: J) => K,
-  ij: (i: I) => J,
-  hi: (g: H) => I,
-  gh: (g: G) => H,
-  fg: (f: F) => G,
-  ef: (e: E) => F,
-  de: (d: D) => E,
-  cd: (c: C) => D,
-  bc: (b: B) => C,
-  ab: (a: A) => B,
-) =>
-(a: A): K => jk(ij(hi(gh(fg(ef(de(cd(bc(ab(a))))))))));
-
-export const compose11 = <A, B, C, D, E, F, G, H, I, J, K, L>(
-  kl: (k: K) => L,
-  jk: (j: J) => K,
-  ij: (i: I) => J,
-  hi: (g: H) => I,
-  gh: (g: G) => H,
-  fg: (f: F) => G,
-  ef: (e: E) => F,
-  de: (d: D) => E,
-  cd: (c: C) => D,
-  bc: (b: B) => C,
-  ab: (a: A) => B,
-) =>
-(a: A): L => kl(jk(ij(hi(gh(fg(ef(de(cd(bc(ab(a)))))))))));
+/**
+ * Given some value and up to 11 function pipe creates a "pipeline", from right
+ * to left,  where the output of a function becomes the input to the next.
+ *
+ * @typeParam A The initial input value and input to the first function
+ * @typeParam B The output of the first function, input to the second
+ * @typeParam C The output of the second function, input to the third function if given, otherwise the return value
+ * @typeParam D The output of the third function, input to the fourth function if given, otherwise the return value
+ * @typeParam E The output of the fourth function, input to the fifth function if given, otherwise the return value
+ * @typeParam F The output of the fifth function, input to the sixth function if given, otherwise the return value
+ * @typeParam G The output of the sixth function, input to the seventh function if given, otherwise the return value
+ * @typeParam H The output of the seventh function, input to the eighth function if given, otherwise the return value
+ * @typeParam I The output of the eighth function, input to the ninth function if given, otherwise the return value
+ * @typeParam J The output of the ninth function, input to the tenth function if given, otherwise the return value
+ * @typeParam K The output of the tenth function, input to the eleventh function if given, otherwise the return value
+ * @typeParam L The output of the eleventh function, the return value
+ * @param {...(a) => b} fns to compose
+ * @returns A value transformed by the functions in the pipeline
+ *
+ * @example
+ * ```ts
+ * import { compose } from '@gimme/base/compose'
+ * const toLower = (s:string):string => s.toLowerCase()
+ * const split = (delimiter: string) => (s: string): string[] => s.split(delimiter)
+ * const join = (delimiter: string) => (s: string[]): string => s.join(delimiter)
+ * const snake_case = compose(join("_"), split(" "), toLower)("Hello world")
+ * ```
+ */
+export function compose<A, B>(
+  ...fns: [(a: A) => B]
+): (a: A) => B;
+export function compose<A, B, C>(
+  ...fns: [(b: B) => C, (a: A) => B]
+): (a: A) => C;
+export function compose<A, B, C, D>(
+  ...fns: [(c: C) => D, (b: B) => C, (a: A) => B]
+): (a: A) => D;
+export function compose<A, B, C, D, E>(
+  ...fns: [(d: D) => E, (c: C) => D, (b: B) => C, (a: A) => B]
+): (a: A) => E;
+export function compose<A, B, C, D, E, F>(
+  ...fns: [(e: E) => F, (d: D) => E, (c: C) => D, (b: B) => C, (a: A) => B]
+): (a: A) => F;
+export function compose<A, B, C, D, E, F, G>(
+  ...fns: [
+    (f: F) => G,
+    (e: E) => F,
+    (d: D) => E,
+    (c: C) => D,
+    (b: B) => C,
+    (a: A) => B,
+  ]
+): (a: A) => G;
+export function compose<A, B, C, D, E, F, G, H>(
+  ...fns: [
+    (g: G) => H,
+    (f: F) => G,
+    (e: E) => F,
+    (d: D) => E,
+    (c: C) => D,
+    (b: B) => C,
+    (a: A) => B,
+  ]
+): (a: A) => H;
+export function compose<A, B, C, D, E, F, G, H, I>(
+  ...fns: [
+    (h: H) => I,
+    (g: G) => H,
+    (f: F) => G,
+    (e: E) => F,
+    (d: D) => E,
+    (c: C) => D,
+    (b: B) => C,
+    (a: A) => B,
+  ]
+): (a: A) => I;
+export function compose<A, B, C, D, E, F, G, H, I, J>(
+  ...fns: [
+    (i: I) => J,
+    (h: H) => I,
+    (g: G) => H,
+    (f: F) => G,
+    (e: E) => F,
+    (d: D) => E,
+    (c: C) => D,
+    (b: B) => C,
+    (a: A) => B,
+  ]
+): (a: A) => J;
+export function compose<A, B, C, D, E, F, G, H, I, J, K>(
+  ...fns: [
+    (j: J) => K,
+    (i: I) => J,
+    (h: H) => I,
+    (g: G) => H,
+    (f: F) => G,
+    (e: E) => F,
+    (d: D) => E,
+    (c: C) => D,
+    (b: B) => C,
+    (a: A) => B,
+  ]
+): (a: A) => K;
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L>(
+  ...fns: [
+    (k: K) => L,
+    (j: J) => K,
+    (i: I) => J,
+    (h: H) => I,
+    (g: G) => H,
+    (f: F) => G,
+    (e: E) => F,
+    (d: D) => E,
+    (c: C) => D,
+    (b: B) => C,
+    (a: A) => B,
+  ]
+): (a: A) => L;
+export function compose(...fns: Array<(x: unknown) => unknown>) {
+  if (fns.length === 0) return <A>(x: A) => x;
+  return fns.reduce((f, g) => (x) => f(g(x)));
+}
--- base/flow.ts
+++ base/flow.ts
@@ -1,3 +1,51 @@
+/**
+ * Given up to 11 function flow creates a "pipeline", from left
+ * to right,  where the output of a function becomes the input to the next.
+ * However, in difference to `pipe`, `flow` takes it's data last. Which is
+ * useful when creating pipeline that can be called later in a point free
+ * style.
+ *
+ * @typeParam A The input to the first function
+ * @typeParam B Result of the first and input to the second function, or result if only one function in the pipeline
+ * @typeParam C Result of the second and input to the third function, or result if only two functions in the pipeline
+ * @typeParam D Result of the third and input to the fourth function, or result if only three functions in the pipeline
+ * @typeParam E Result of the fourth and input to the fifth function, or result if only four functions in the pipeline
+ * @typeParam F Result of the fifth and input to the sixth function, or result if only five functions in the pipeline
+ * @typeParam G Result of the sixth and input to the seventh function, or result if only six functions in the pipeline
+ * @typeParam H Result of the seventh and input to the eight function, or result if only seven functions in the pipeline
+ * @typeParam I Result of the eight and input to the ninth function, or result if only eight functions in the pipeline
+ * @typeParam J Result of the ninth and input to the tenth function, or result if only nine functions in the pipeline
+ * @typeParam K Result of the tenth and input to the eleventh function, or result if only ten functions in the pipeline
+ * @typeParam L Result of the eleventh function
+ * @param a The input value
+ * @param ab function to transform input A to output B,
+ * @param bc function to transform input B to output C,
+ * @param cd function to transform input C to output D,
+ * @param de function to transform input D to output E,
+ * @param ef function to transform input E to output F,
+ * @param fg function to transform input F to output G,
+ * @param gh function to transform input G to output H,
+ * @param hi function to transform input H to output I,
+ * @param ij function to transform input I to output J,
+ * @param jk function to transform input J to output K,
+ * @param kl function to transform input K to output L,
+ * @returns A value transformed by the functions in the pipeline
+ *
+ * @example
+ * ```ts
+ * import { flow } from '@gimme/base/flow'
+ *
+ * const toLower = (s:string):string => s.toLowerCase()
+ * const split = (delimiter: string) => (s: string): string[] => s.split(delimiter)
+ * const join = (delimiter: string) => (s: string[]): string => s.join(delimiter)
+ *
+ * const hyphen = flow(toLower, split(" "), join("-"))
+ * const snake = flow(toLower, split(" "), join("_"))
+ *
+ * const hyphenCase = hyphen("Hello World")
+ * const snakeCase = snake("Hello World")
+ * ```
+ */
 export function flow<A, B>(ab: (a: A) => B): (a: A) => B;
 export function flow<A, B, C>(ab: (a: A) => B, bc: (b: B) => C): (a: A) => C;
 export function flow<A, B, C, D>(
